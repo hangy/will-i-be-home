a31090c51138aba6c22bef8b2ef2d48b0a837a88
hangy <hangy@hangy.de>
2021-11-18 01:18:52 +0100

    refactor: Use minimal API, because the rest is overkill for this single REST call

--- WillIBeHome.Api/Program.cs
+++ WillIBeHome.Api/Program.cs
@@ -1,16 +1,44 @@
-namespace WillIBeHome.Api;
+using System.Net;
+using Microsoft.Extensions.ML;
+using WillIBeHome.Api;
+using WillIBeHome.ML;
+using WillIBeHome.Owntracks;
+using WillIBeHome.Shared;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+OwntracksSettings? owntracksSettings = builder.Configuration.GetSection("Owntracks").Get<OwntracksSettings>();
+builder.Services.AddHttpClient<IOwntracksApiClient, OwntracksApiClient>(c =>
 {
-    public static void Main(string[] args)
+    c.BaseAddress = owntracksSettings.Uri;
+}).ConfigurePrimaryHttpMessageHandler(_ => new HttpClientHandler { Credentials = new NetworkCredential(owntracksSettings.HttpUserName, owntracksSettings.HttpPassword) });
+
+MLSettings? mlSettings = builder.Configuration.GetSection("ML").Get<MLSettings>();
+builder.Services.AddPredictionEnginePool<Transition, WillBeHomePrediction>()
+    .FromFile(mlSettings.ModelPath);
+
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.MapGet("/prediction", async (string user, string device, IOwntracksApiClient owntracksApiClient, PredictionEnginePool<Transition, WillBeHomePrediction> predictionEnginePool, CancellationToken cancellationToken) =>
+{
+    GetLocationsResult? locations = await owntracksApiClient.GetLocationsAsync(user, device, DateTimeOffset.UtcNow.AddDays(-1), cancellationToken: cancellationToken).ConfigureAwait(false);
+    if (locations == null)
     {
-        CreateHostBuilder(args).Build().Run();
+        return false;
     }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
-}
+    IEnumerable<Transition>? transitions = LocationsToTransitionsConverter.Convert(locations.Data.Select(l => LocationConverter.Convert(user, device, l)));
+    Transition? lastTransition = transitions.LastOrDefault();
+    if (lastTransition == null)
+    {
+        return false;
+    }
+
+    WillBeHomePrediction? predictionResult = predictionEnginePool.Predict(lastTransition);
+    return predictionResult.Label;
+})
+.WithName("GetPrediction");
+
+app.Run();
